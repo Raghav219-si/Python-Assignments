a426e22d562ee6ce855ed2d2a989d2e9c6cd1371
Harshit Prasad <harshit.codepy@gmail.com>
2016-12-19 14:25:33 +0530

    ASSIGNMENT 10

---
+++ slice.py
@@ -0,0 +1,12 @@
+# We are going to initialize a string.
+name = 'Harshit Prasad'
+print(name[:7]) # Starts from starting and printing ends at position 6
+print(name[3:7]) # Starts from 3 and ends before 7
+print(name) # Prints out the whole string.
+print(name[:]) # Prints out the whole string.
+print(name[::2]) # Here it means that from that starting leave 1 letter and print next letter.
+
+print(name[::-1]) # Reverses the string.
+# Here -1 means that it is saying don't leave a letter, and start from reverse order.
+
+# This is the concept of slicing of the strings in Python.
