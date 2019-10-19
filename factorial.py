0ea272cb9d3c6011c06c7a45abd23f7793272d05
Raghav219-si <55808433+Raghav219-si@users.noreply.github.com>
2019-10-20 01:10:32 +0530

    added a shortcut
    a direct output for zero as input

--- factorial.py
+++ factorial.py
@@ -3,15 +3,13 @@
 # Python program which calculates the factorial of a number
 
 def factorial(n): # Here we will be going to use recursion method
-    if n==1:
+    if n==1 or n==0 :
         return 1
     else:
         return n*factorial(n-1)
 
 number = int(input("Enter the number of which factorial is to be calculated-"))
-if number == 0:
-    print("Factorial of 0 is 1.")
-elif number < 0:
+if number < 0:
     print("Factorial of a negative number does not exist.")
 else:
     print("Factorial of the", number, "is", factorial(number))
